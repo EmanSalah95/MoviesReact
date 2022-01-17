36d0fca70abe656d12986fae0b74cf10b80821b7
EmanSalah95 <seman9918@gmail.com>
2022-01-17 20:11:21 +0200

    addContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Favourites from './pages/Favourites/Favourites';
+import Favourites from "./pages/Favourites/Favourites";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { LangProvider } from "./context/langContext";
@@ -10,18 +10,17 @@ function App() {
   const [lang, setLang] = useState("en");
   return (
     <Provider store={store}>
-    <Router>
-      <LangProvider value={{ lang, setLang }}>
-        <div dir={lang == "en" ? "ltr" : "rtl"} >
-          <Switch>
-          <Route path="/favourite" exact component={Favourites} />
-          <Route path="/" exact component={Home} />
-          </Switch>
-        </div>
-      </LangProvider>
-    </Router>
-  </Provider>
-    
+      <Router>
+        <LangProvider value={{ lang, setLang }}>
+          <div dir={lang == "en" ? "ltr" : "rtl"}>
+            <Switch>
+              <Route path="/favourite" exact component={Favourites} />
+              <Route path="/" exact component={Home} />
+            </Switch>
+          </div>
+        </LangProvider>
+      </Router>
+    </Provider>
   );
 }
 
